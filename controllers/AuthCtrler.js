6d2a09259e13e83dcf74aaddcc1d3b92e625db4a
Tin98 <truongdaitin98@gmail.com>
2019-12-24 19:11:59 +0700

    forgot password

--- controllers/AuthCtrler.js
+++ controllers/AuthCtrler.js
@@ -1,191 +1,203 @@
 var User = require('../models/User');
 var Mailer = require('../utils/Mailer');
-var passport = require("passport");
-var jwt = require("jsonwebtoken");
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
 
 exports.login = async (req, res, next) => {
-    passport.authenticate('local', {session: false}, (err, user) => {
-        if (err || !user) {
-            // Email or Password is wrong
-            let error = {
-                status: 400,
-                code: 1001,
-                message: "Fail"
-            };
-            return res.status(400).json({ data: {}, error });
-        }
-        req.login(user, {session: false}, (err) => {
-            jwt.sign({ user: user }, "StRoNGs3crE7", (err, token) => {
-                if (err) {
-                    console.log(err);
-                    let error = {
-                        status: 500,
-                        code: 1002,
-                        message: "Fail"
-                    };
-                    return res.status(500).json({ data: {}, error });
-                }
-                res.cookie("jwt", token, {
-                    httpOnly: true,
-                    sameSite: true
-                });
-                console.log(user);
-                return res.json({
-                    status: 200,
-                    code: 1,
-                    message: "Succeed",
-                    data: user
-                });
-            });
-        });
-    })(req, res);
+	passport.authenticate('local', { session: false }, (err, user) => {
+		if (err || !user) {
+			// Email or Password is wrong
+			let error = {
+				status: 400,
+				code: 1001,
+				message: 'Fail'
+			};
+			return res.status(400).json({ data: {}, error });
+		}
+		req.login(user, { session: false }, (err) => {
+			jwt.sign({ user: user }, 'StRoNGs3crE7', (err, token) => {
+				if (err) {
+					console.log(err);
+					let error = {
+						status: 500,
+						code: 1002,
+						message: 'Fail'
+					};
+					return res.status(500).json({ data: {}, error });
+				}
+				res.cookie('jwt', token, {
+					httpOnly: true,
+					sameSite: true
+				});
+				console.log(user);
+				return res.json({
+					status: 200,
+					code: 1,
+					message: 'Succeed',
+					data: user
+				});
+			});
+		});
+	})(req, res);
 };
 
 exports.register = async (req, res, next) => {
-    if (!req.body.name || !req.body.email || !req.body.password || req.body.password !== req.body.repassword) {
-        let error = {
-            status: 400,
-            code: 1003,
-            message: "Fail"
-        };
-        return res.status(400).json({ data: {}, error });
-    }
-    if (!await User.findByEmail(req.body.email)) {
-        let newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        });
-        let user = await newUser.save();
-        if (!user) {
-            let error = {
-                status: 500,
-                code: 1002,
-                message: "Fail"
-            };
-            return res.status(500).json({ data: {}, error });
-        }
-    }
-    this.login(req, res, next);
+	if (!req.body.name || !req.body.email || !req.body.password || req.body.password !== req.body.repassword) {
+		let error = {
+			status: 400,
+			code: 1003,
+			message: 'Fail'
+		};
+		return res.status(400).json({ data: {}, error });
+	}
+	if (!await User.findByEmail(req.body.email)) {
+		let newUser = new User({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password
+		});
+		let user = await newUser.save();
+		if (!user) {
+			let error = {
+				status: 500,
+				code: 1002,
+				message: 'Fail'
+			};
+			return res.status(500).json({ data: {}, error });
+		}
+	}
+	this.login(req, res, next);
 };
 
 exports.checkEmailExisting = async (req, res, next) => {
-    User.findByEmail(req.query.email).then(user => {
-        if (user) {
-            res.json(true);
-        } else {
-            res.json(false);
-        }
-    });
+	User.findByEmail(req.query.email).then((user) => {
+		if (user) {
+			res.json(true);
+		} else {
+			res.json(false);
+		}
+	});
 };
 
 exports.forgotPassword = async (req, res, next) => {
-    console.log(req.body);
-    // TODO:
-
-    res.render('index', { title: 'GoDeam Toy World' });
-
+	console.log(req.body);
+	let email = req.body.email;
+	//const password_random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+	User.findByEmail(email).then((user) => {
+		temp_password = user.genTempPassword();
+		if (user) {
+			Mailer.sendForgotPasswordMail(email, temp_password)
+				.then(() => {
+					res.send('<h1>Đã gửi đến email ' + email + '<h1>');
+				})
+				.catch((err) => {
+					console.log(err);
+					res.send('<h1>Lỗi gửi email, liên hệ admin để fix lỗi' + '<h1>');
+				});
+		} else {
+			res.send('<h1>Email ' + email + ' bạn nhập không tồn tại' + '<h1>');
+		}
+	});
 };
 
 exports.logout = async (req, res, next) => {
-    res.clearCookie("jwt");
-    res.removeHeader("Cookie");
-    res.json(true);
+	res.clearCookie('jwt');
+	res.removeHeader('Cookie');
+	res.json(true);
 };
 
 exports.profile = async (req, res, next) => {
-    console.log(req.body);
-    // TODO:
+	console.log(req.body);
+	// TODO:
 
-    res.render('index', { title: 'GoDeam Toy World' });
-
+	res.render('index', { title: 'GoDeam Toy World' });
 };
 
 exports.editInfo = async (req, res, next) => {
-    if (!req.body.name || !req.body.email) {
-        let error = {
-            status: 400,
-            code: 1003,
-            message: "Fail"
-        };
-        return res.status(400).json({ data: {}, error });
-    }
-    if (await User.findByEmail(req.body.email) && req.body.email !== res.locals.user.email) {
-        let error = {
-            status: 400,
-            code: 1003,
-            message: "Fail" // Email has been taken
-        };
-        return res.status(400).json({ data: {}, error });
-    }
-    User.findByEmail(res.locals.user.email).then(user => {
-        if (!user) {
-            let error = {
-                status: 400,
-                code: 1003,
-                message: "Fail"
-            };
-            return res.status(400).json({ data: {}, error });
-        }
-        user.name = req.body.name;
-        user.email = req.body.email;
-        user.phone = req.body.phone;
-        user.address = req.body.address;
-        user.save().then(updatedUser => {
-            if (!updatedUser) {
-                let error = {
-                    status: 500,
-                    code: 1005,
-                    message: "Fail"
-                };
-                return res.status(500).json({ data: {}, error });
-            } else {
-                return res.json({
-                    status: 200,
-                    code: 1,
-                    message: "Succeed",
-                    data: user
-                });
-            }
-        });
-    });
+	if (!req.body.name || !req.body.email) {
+		let error = {
+			status: 400,
+			code: 1003,
+			message: 'Fail'
+		};
+		return res.status(400).json({ data: {}, error });
+	}
+	if ((await User.findByEmail(req.body.email)) && req.body.email !== res.locals.user.email) {
+		let error = {
+			status: 400,
+			code: 1003,
+			message: 'Fail' // Email has been taken
+		};
+		return res.status(400).json({ data: {}, error });
+	}
+	User.findByEmail(res.locals.user.email).then((user) => {
+		if (!user) {
+			let error = {
+				status: 400,
+				code: 1003,
+				message: 'Fail'
+			};
+			return res.status(400).json({ data: {}, error });
+		}
+		user.name = req.body.name;
+		user.email = req.body.email;
+		user.phone = req.body.phone;
+		user.address = req.body.address;
+		user.save().then((updatedUser) => {
+			if (!updatedUser) {
+				let error = {
+					status: 500,
+					code: 1005,
+					message: 'Fail'
+				};
+				return res.status(500).json({ data: {}, error });
+			} else {
+				return res.json({
+					status: 200,
+					code: 1,
+					message: 'Succeed',
+					data: user
+				});
+			}
+		});
+	});
 };
 
 exports.editPassword = async (req, res, next) => {
-    if (!req.body.password || req.body.password !== req.body.repassword) {
-        let error = {
-            status: 400,
-            code: 1003,
-            message: "Fail"
-        };
-        return res.status(400).json({ data: {}, error });
-    }
-    User.findByEmail(res.locals.user.email).then(user => {
-        if (!user) {
-            let error = {
-                status: 400,
-                code: 1003,
-                message: "Fail"
-            };
-            return res.status(400).json({ data: {}, error });
-        }
-        user.setPassword(req.body.password);
-        user.save().then(updatedUser => {
-            if (!updatedUser) {
-                let error = {
-                    status: 500,
-                    code: 1005,
-                    message: "Fail"
-                };
-                return res.status(500).json({ data: {}, error });
-            } else {
-                return res.json({
-                    status: 200,
-                    code: 1,
-                    message: "Succeed",
-                    data: user
-                });
-            }
-        });
-    });
+	if (!req.body.password || req.body.password !== req.body.repassword) {
+		let error = {
+			status: 400,
+			code: 1003,
+			message: 'Fail'
+		};
+		return res.status(400).json({ data: {}, error });
+	}
+	User.findByEmail(res.locals.user.email).then((user) => {
+		if (!user) {
+			let error = {
+				status: 400,
+				code: 1003,
+				message: 'Fail'
+			};
+			return res.status(400).json({ data: {}, error });
+		}
+		user.setPassword(req.body.password);
+		user.save().then((updatedUser) => {
+			if (!updatedUser) {
+				let error = {
+					status: 500,
+					code: 1005,
+					message: 'Fail'
+				};
+				return res.status(500).json({ data: {}, error });
+			} else {
+				return res.json({
+					status: 200,
+					code: 1,
+					message: 'Succeed',
+					data: user
+				});
+			}
+		});
+	});
 };
--- controllers/ShopCtrler.js
+++ controllers/ShopCtrler.js
@@ -1,98 +1,89 @@
 var User = require('../models/User');
-var Product = require('../models/Product'); 
+var Product = require('../models/Product');
 var Category = require('../models/Category');
 _logined = false;
 
 exports.homeShow = async (req, res, next) => {
-    let cates = await Category.find({});
-    let products = await Product.find({}).sort({ updateAt: -1, price: -1 }).limit(12);
-    
-    
-    res.render('index', { categories : cates, products});
+	let cates = await Category.find({});
+	let products = await Product.find({}).sort({ updateAt: -1, price: -1 }).limit(12);
+
+	res.render('index', { categories: cates, products });
 };
 
 exports.shopShow = async (req, res, next) => {
-    let cates = await Category.find({});
-    let products;
-    let titleToy;
+	let cates = await Category.find({});
+	let products;
+	let titleToy;
 
-    if (!req.query.category) {
-        titleToy = "Đồ chơi";
-        products = await Product.find({});
-    } else {
-        let cateName = await Category.findOne({ 'link': req.query.category });
-        products = await Product.find({ 'category' : cateName.name });
-        titleToy = cateName.name;
-    }
-    
-    res.render('shop', { categories : cates, products, titleToy });
+	if (!req.query.category) {
+		titleToy = 'Đồ chơi';
+		products = await Product.find({});
+	} else {
+		let cateName = await Category.findOne({ link: req.query.category });
+		products = await Product.find({ category: cateName.name });
+		titleToy = cateName.name;
+	}
+
+	res.render('shop', { categories: cates, products, titleToy });
 };
 
 exports.searchShow = async (req, res, next) => {
-    let cates = await Category.find({});
-    let products;
+	let cates = await Category.find({});
+	let products;
 
-    if (!req.query.search) {
-        products = await Product.find({});
-    } else {
-        products = await Product.find({ 'name' : { $regex: req.query.search } });
-    }
-    
-    res.render('shop', { categories : cates, products });
+	if (!req.query.search) {
+		products = await Product.find({});
+	} else {
+		products = await Product.find({ name: { $regex: req.query.search } });
+	}
+
+	res.render('shop', { categories: cates, products });
 };
 
 exports.productDetail = async (req, res, next) => {
-    let cates = await Category.find({});
-    let product = await Product.findById(req.params.id);
-    
-    if (!product) res.redirect('/shop');
-    res.render('single', { categories : cates, product });
+	let cates = await Category.find({});
+	let product = await Product.findById(req.params.id);
+
+	if (!product) res.redirect('/shop');
+	res.render('single', { categories: cates, product });
 };
 
 exports.cart = async (req, res, next) => {
-    let cates = await Category.find({});
-    // TODO:
-    
-    res.render('index', { title: 'GoDeam Toy World' });
+	let cates = await Category.find({});
+	// TODO:
 
+	res.render('index', { title: 'GoDeam Toy World' });
 };
 
 exports.order = async (req, res, next) => {
-    let cates = await Category.find({});
-    // TODO:
-    
-    res.render('index', { title: 'GoDeam Toy World' });
+	let cates = await Category.find({});
+	// TODO:
 
+	res.render('index', { title: 'GoDeam Toy World' });
 };
 
 exports.contact = async (req, res, next) => {
-    let cates = await Category.find({});
-    
-    res.render('contact', { categories : cates });
+	let cates = await Category.find({});
 
+	res.render('contact', { categories: cates });
 };
 
 exports.checkout = async (req, res, next) => {
-    console.log(req.body);
-    let quantity = await req.body.quantity;
-    let id = await req.body.item_number;
-    let products = await Product.find().where('_id').in(id).exec();
-    let cates = await Category.find({});
-    res.render('checkout', { categories : cates, quantity, products});
-
+	console.log(req.body);
+	let quantity = await req.body.quantity;
+	let id = await req.body.item_number;
+	let products = await Product.find().where('_id').in(id).exec();
+	let cates = await Category.find({});
+	res.render('checkout', { categories: cates, quantity: quantity, products: products });
 };
 exports.payment = async (req, res, next) => {
-    let cates = await Category.find({});
-    res.render('payment', { categories : cates });
-
+	let cates = await Category.find({});
+	res.render('payment', { categories: cates });
 };
 exports.single = async (req, res, next) => {
-    let cates = await Category.find({});
+	let cates = await Category.find({});
 
-    
-    let product = await Product.findById(req.query.id);
-    
-   
-    res.render('single', { categories : cates, product });
+	let product = await Product.findById(req.query.id);
 
+	res.render('single', { categories: cates, product });
 };
